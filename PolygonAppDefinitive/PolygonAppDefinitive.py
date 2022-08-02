c4b0c3594127ed21036c5fb2b5536411896b7c8a
gabriros <62215277+gabriros@users.noreply.github.com>
2022-08-02 19:13:53 +0200

    alg

--- PolygonAppDefinitive/PolygonAppDefinitive.py
+++ PolygonAppDefinitive/PolygonAppDefinitive.py
@@ -8,18 +8,22 @@ from tkinter.colorchooser import askcolor
 from PIL import Image, ImageTk
 from random import seed
 from random import randint
+import sys
+import math
+import matplotlib.pyplot as plt
+import 
 
 
 
-a = 752
-b = 1504
+screenH = 752
+screenW = 1504
 user32 = ctypes.windll.user32
 if(user32.GetSystemMetrics(0) == 2560 and user32.GetSystemMetrics(1) == 1440):
-    a=1000
-    b=2000 
+    screenH=1000
+    screenW=2000 
 elif(user32.GetSystemMetrics(0) == 1280 and user32.GetSystemMetrics(1) == 720):
-    a=501
-    b=1003
+    screenH=501
+    screenW=1003
 
 colors = [(126, 150, 194), '#7e96c2']
 
@@ -67,6 +71,47 @@ l = tk.IntVar();
 
 
 #Methods
+def random_partition(a):
+    b = list()
+    c = list()
+    for i in range(len(a)):
+        if random.randrange(2):
+            b.append(a[i])
+        else:
+            c.append(a[i])
+    return b, c
+
+def get_deltas(n):
+    de = [random.random() for _ in range(n)]
+    print('\n', de)
+    de.sort()
+    dep, dem = random_partition(de[1:-1])
+    dem.reverse()
+    des = [de[0]] + dep + [de[-1]] + dem + [de[0]]
+    deltas = [des[i] - des[i-1] for i in range(1, len(des))]
+    return deltas, (de[0], de[-1])
+
+def get_xyq(n, x, y):
+    print (x)
+    print( '\n' '\n')
+    
+    x, (a1, a2) = get_deltas(n)
+    y, (b1, b2) = get_deltas(n)
+    print (x)
+    random.shuffle(y)
+    vectors = [(x[i], y[i]) for i in range(n)]
+    vectors.sort(key=lambda v: math.atan2(v[1], v[0]))
+    points = [(0, 0)]
+    for v in vectors:
+        points.append((points[-1][0] + v[0], points[-1][1] + v[1]))
+    xmin = min([p[0] for p in points])
+    ymin = min([p[1] for p in points])
+    dx = a1 - xmin
+    dy = b1 - ymin
+    points = np.array([(p[0]+dx, p[1]+dy) for p in points])
+    print("\n", points)
+    return points
+
 def optionsFrame():
     MenuPage.pack_forget()
     OptionsPage.pack(expand = True, fill = "both")
@@ -94,97 +139,54 @@ def exit():
 
 def draw_grid():
     c.delete("all")
-    hGap = b / gridDimension.get() #coordinate griglia
-    vGap = a / gridDimension.get()
+    hGap = screenW / gridDimension.get() #coordinate griglia
+    vGap = screenH / gridDimension.get()
 
     h_coord= []
     v_coord=[]
     n=1
 
-    lenghtx = 0
-    lenghty = 0
-    middlex = 0
-    middley = 0
-
     #coordinate x e y
     y = []
     x = []
 
-    #valori estremi x e y
-    epx = [16,19]
-    epy = [17,18]
-
-    #divisione valori interni random
-    y1 = []
-    y2 = []
-    x1 = []
-    x2 = []
-
-    #vector
-    vect1 = [],[]
-
-    dim = gridDimension.get() * gridDimension.get()
-   
-
     # Creates all vertical lines at intevals of hGap
-    for i in range(0, b, int(hGap)):
-        c.create_line(i, 0, i, a, fill="black")
+    for i in range(0, screenW, int(hGap)):
+        c.create_line(i, 0, i, screenH, fill="black")
         h_coord.append(i)
     # Creates all horizontal lines at intevals of vGap
-    for i in range(0, a, int(vGap)):
-        c.create_line(0, i, b, i, fill="black")
+    for i in range(0, screenH, int(vGap)):
+        c.create_line(0, i, screenW, i, fill="black")
         v_coord.append(i)
-
-    #i numeri dei lati devono essere pari. metto controllo per non farmi rompere
-    while(n%2) != 0:
-         n = np.random.randint(0, dim) #numero di lati del poligono
-
-    for i in range(0,n):
-        x.append(random.choice(h_coord))
-        y.append(random.choice(v_coord))
     
-    #li ordino
-    x.sort()
-    y.sort()
+    dim = gridDimension.get() * gridDimension.get()
 
-    #isolo punti estremi epx 
-    epx [0] = x[0]
-    epx [1] = x[len(x) - 1]
-    
-    #elimino punti estremi da x
-    del x[0]
-    del x[len(x) - 1]
-    
-    #isolo punti estremi epy
-    epy [0] = y[0]
-    epy [1] = y[len(y) - 1]
+    #Inizio algoritmo
+    if len(sys.argv) == 2:
+        dim = int(sys.argv[1])
+    points = get_xyq(dim, h_coord, v_coord)
 
-    #elimino punti estremi da y
-    del y[0]
-    del y[len(y) - 1]
-    
-    #divido casualmente i punti interni in 2 catene
-    random.shuffle(x)
-    random.shuffle(y)
+    vector1 = []
+    vector2 = []
+    #split points into two different arrays
+    for x, y in points:
+        vector1.append(x)
+        vector2.append(y)
 
-    lenghtx = len(x)
-    middlex = (len(x)//2)
-    x1 = x[:middlex]
-    x2 = x[middlex:]
-    
+    vector1 = vector1.inverse_transform
+    vector2 = vector2 * 1000
+    print("\n", vector1)
+    print("\n", vector2)
+    qwerty = len(vector1)
 
-    lenghty = (len(y))
-    middley = (len(y)//2)
-    y1 = y[0:middley]
-    y2 = y[middley:]
-
-    #accoppio casualmente le coordinate
-    
-
-
-    c.create_polygon(list(y1),list(x1), outline='#f11', fill='red', width=2)
-    
+    if (qwerty %2 != 0):
+        vector1 = vector1.pop()
+    for q in range(0, 1, len(vector1)):
+        c.create_polygon(vector1[q],vector2[q], outline='#f11', fill='red', width=2)
         
+    
+    #c.create_line(p[0] for p in points], [p[1] for p in points], outline='#f11', fill='red', width=2)
+     
 
 def check_hand_enter():
     c.config(cursor="hand1")
@@ -211,7 +213,7 @@ backgroundButton.grid(column=0, row=0)
 optBackButton.grid(column=0, row=1)
 
 #Game Widgets
-c = Canvas(GamePage, bg="white", height=a, width=b)
+c = Canvas(GamePage, bg="white", height=screenH, width=screenW)
 tag_name = "polygon"
 c.create_polygon((100, 100), (25, 100), (125, 100), (125, 25), outline='black', fill="black", tag=tag_name)
 c.tag_bind(tag_name, "<Enter>", lambda event: check_hand_enter())
